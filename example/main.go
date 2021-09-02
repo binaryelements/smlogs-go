4fde7b2c797507b02db8ff5577aad367bb28105b
Binary Elements <martin.wong@binaryelements.com.au>
2021-09-02 18:36:37 +1000

    updated example to properly show usage

--- example/main.go
+++ example/main.go
@@ -1,9 +1,10 @@
 package main
 
-import "github.com/binaryelements/SMLogs"
+import SMLogs "github.com/binaryelements/smlogs-go"
 
 var SMLogger SMLogs.Config
 
 func main() {
 	SMLogger.New("ABC-PUBLIC-API", "fd3tgg-8081-4d4d5-bfed-431cc3ea9eb82", "https://abclogs.smoothorders.com", "DEBUG", "Y", "Y")
+	SMLogger.Error("It's an error!")
 }
